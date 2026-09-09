0172d113d80b474eb7c4000f8ecfa411c9e16075
SalzDevs <a49000@alunos.isel.pt>
2026-09-09 21:24:07 +0100

    chore: add .gitignore, untrack build artifacts and target dir

--- apps/desktop/src-tauri/target/debug/build/proc-macro2-5a3e6cf60574dd1c/build_script_build-5a3e6cf60574dd1c.d
+++
@@ -1,5 +0,0 @@
-C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\apps\desktop\src-tauri\target\debug\build\proc-macro2-5a3e6cf60574dd1c\build_script_build-5a3e6cf60574dd1c.d: C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\.tools\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.107\build.rs
-
-C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\apps\desktop\src-tauri\target\debug\build\proc-macro2-5a3e6cf60574dd1c\build_script_build-5a3e6cf60574dd1c.exe: C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\.tools\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.107\build.rs
-
-C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\.tools\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\proc-macro2-1.0.107\build.rs:
--- apps/desktop/src-tauri/target/debug/deps/fastrand-fec58be3b1cf3992.d
+++
@@ -1,7 +0,0 @@
-C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\apps\desktop\src-tauri\target\debug\deps\fastrand-fec58be3b1cf3992.d: C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\.tools\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fastrand-2.5.0\src\lib.rs
-
-C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\apps\desktop\src-tauri\target\debug\deps\libfastrand-fec58be3b1cf3992.rlib: C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\.tools\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fastrand-2.5.0\src\lib.rs
-
-C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\apps\desktop\src-tauri\target\debug\deps\libfastrand-fec58be3b1cf3992.rmeta: C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\.tools\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fastrand-2.5.0\src\lib.rs
-
-C:\Users\rceia\Downloads\ServiceNow-UpdateSet-UserStory\uigrep\.tools\cargo\registry\src\index.crates.io-1949cf8c6b5b557f\fastrand-2.5.0\src\lib.rs:
